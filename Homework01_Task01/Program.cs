56d3a59bc71e54dc3a9597a554223ba5165c0b51
Dimoklim <dimoklim@ya.ru>
2022-04-17 12:14:59 +0300

    Add Homework01 Task01

---
+++ Homework01_Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1. Заказчик предложил работать с целыми числами и выводить только максимальное
+int a = 5;
+int b = 7;
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}
