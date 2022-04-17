6062db5ab1d293538063cbd16cb2fe497e54eaaa
Dimoklim <dimoklim@ya.ru>
2022-04-17 19:29:46 +0300

    Add Homework01 Task02

---
+++ Homework01_Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 2. Найти наибольшее из 3 целых чисел.
+int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.Write(max);
