6100618c1bbe2400a7711bc988c801d10e21a75e
Dimoklim <dimoklim@ya.ru>
2022-04-17 12:00:06 +0300

    Homework01_Task01

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Заказчик не уточнил необходимость проверки других дней недели
+
+int DayOfWeek = 3;
+if (DayOfWeek == 3) 
+{ 
+    Console.WriteLine("Среда");
+}
+if (DayOfWeek == 5) 
+{
+    Console.WriteLine("Пятница");
+}
