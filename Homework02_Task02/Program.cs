4949868da0427b672d90ef16893eaab6bf319a6f
Dimoklim <dimoklim@ya.ru>
2022-04-20 07:46:57 +0300

    Add Homework02 Task02

---
+++ Homework02_Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Принимает на вход 3-значное число и выводит вторую цифру этого числа
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+//num = num / 10;
+//num = num % 10;
+//Console.WriteLine(num);
+
+int num = 918;
+if (num > 100 && num < 1000)
+{
+    num = num / 10;
+    num = num % 10;
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine("Число не входит в диапазон");
+}
