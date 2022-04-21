b1a418254daa8bdaadb89e5f4475076ce72679f8
Dimoklim <dimoklim@ya.ru>
2022-04-21 17:54:14 +0300

    Add Homework02 Task08

---
+++ Homework02_Task08/Program.cs
@@ -0,0 +1,6 @@
+﻿// 8 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int DayOfWeek = 7;
+if (DayOfWeek == 6 || DayOfWeek == 7) Console.Write("да");
+if (DayOfWeek > 0 && DayOfWeek < 6) Console.Write("нет");
+if (DayOfWeek <= 0 || DayOfWeek > 7) Console.Write("введите корректный день недели");
