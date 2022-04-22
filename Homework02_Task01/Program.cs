a917fc0b12859175fcdee5a1e4149f37b69242aa
Dimoklim <dimoklim@ya.ru>
2022-04-22 17:29:58 +0300

    Add Homework02 Task01

---
+++ Homework02_Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите метод, который выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10, 100);
+int max = 0;
+
+void MaxDigit()         // Определяем метод
+{
+    int FirstDigit = num / 10;
+    int SecondDigit = num % 10;
+    if (FirstDigit > SecondDigit) max = FirstDigit;
+    else max = SecondDigit;
+    Console.Write ($"Наибольшая цифра числа {num} это {max}");          // Интерполяция
+}
+
+MaxDigit();         // Вызываем метод
