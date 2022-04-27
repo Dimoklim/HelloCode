dd1707e54169a75256ecfd9ce3c00bb0cb9c02fe
Dimoklim <dimoklim@ya.ru>
2022-04-27 23:58:25 +0300

    Add Homework03 Task04

---
+++ Homework03_Task04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int x = 1; 
+int y = -5; 
+ 
+int QuarterNum = 0; 
+if (x > 0 && y > 0) QuarterNum = 1; 
+if (x < 0 && y > 0) QuarterNum = 2; 
+if (x < 0 && y < 0) QuarterNum = 3; 
+if (x > 0 && y < 0) QuarterNum = 4; 
+Console.WriteLine(QuarterNum);
