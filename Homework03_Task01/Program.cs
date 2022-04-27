e03a426774adf5e8c6f6312b1537e85d14e7c3e5
Dimoklim <dimoklim@ya.ru>
2022-04-27 23:57:45 +0300

    Add Homework03 Task01

---
+++ Homework03_Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = 14212;
+
+int FirstDigit = (num / 10000) % 10;
+int SecondDigit = (num / 1000) % 10;
+int FourthDigit = (num / 10) % 10;
+int FithDigit = num % 10;
+
+if (FirstDigit == FithDigit && SecondDigit == FourthDigit) Console.Write("Да");
+else Console.Write("Нет");
