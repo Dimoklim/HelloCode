310c39ae900d4c8e1099ad9694e37440b5a6dfc2
Dimoklim <dimoklim@ya.ru>
2022-05-02 17:56:26 +0300

    Add Homework04 Task25

---
+++ Homework04_Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+
+int SumOfNumbers(int A)
+{
+    int res = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        res = res + i;
+    }
+    return res;
+}
+Console.Write(SumOfNumbers(15));
