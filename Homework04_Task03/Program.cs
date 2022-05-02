6f9749267545db23978e0f85b84d5db72ea79184
Dimoklim <dimoklim@ya.ru>
2022-05-02 19:15:17 +0300

    Add Homework04 Task03

---
+++ Homework04_Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreateRandomArray (int length) // метод создания рандомного массива
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+string PrintArrayToString (int[] array) // метод вывода массива в виде строки
+{
+    return String.Join (',', array);
+}
+Console.Write(PrintArrayToString(CreateRandomArray(8)));
