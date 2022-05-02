4854faabca3b2c8aa41f2fae79906417f1ef6502
Dimoklim <dimoklim@ya.ru>
2022-05-02 19:36:40 +0300

    Edit Homework04 Task25

--- Homework04_Task25/Program.cs
+++ Homework04_Task25/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Найти сумму чисел от 1 до А
 
-int SumOfNumbers(int A)
+int SumOfNumbers(int A) // Способ 1
 {
     int res = 0;
     for (int i = 0; i <= A; i++)
@@ -9,4 +9,8 @@ int SumOfNumbers(int A)
     }
     return res;
 }
-Console.Write(SumOfNumbers(15));
+//Console.Write(SumOfNumbers(15)); 
+
+int A = new Random().Next(2, 100); // Способ 2
+int res = A * (A + 1) / 2;
+Console.Write($"Сумма чисел от 1 до {A} равна {res}");
