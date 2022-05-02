372a9a57aedef5fd0272ba542a1224f7cc5c3adf
Dimoklim <dimoklim@ya.ru>
2022-05-02 18:18:07 +0300

    Add Homework04 Task01

---
+++ Homework04_Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+int A = 2, B = 3;
+int Result = 1;
+for (int i = 1; i <= B; i++)
+{
+    Result = Result * A;
+}
+Console.WriteLine(Result);
