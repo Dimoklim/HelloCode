bdcfcc5710e967a12ce4efbd3cf09c9de379cae8
Dimoklim <dimoklim@ya.ru>
2022-05-02 18:46:58 +0300

    Add Homework04 Task02

---
+++ Homework04_Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = new Random().Next(1, 10000);
+int sum = 0;
+
+Console.Write($"Сумма цифр в числе {number} равна ");
+for (int i = 0; number >= 1; i++)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.Write(sum);
