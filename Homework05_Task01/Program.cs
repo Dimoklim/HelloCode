b183cd5b92e5311b6872777148720b32867abf22
Dimoklim <dimoklim@ya.ru>
2022-05-12 17:27:50 +0300

    Add Homework05 Task01

---
+++ Homework05_Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] createArray(int elements) // метод создания и заполнения массива
+{
+    int[] arr = new int[elements];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintNew(int[] array) // метод вывода массива
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int EvenNum(int[] array) // метод нахождения кол-ва четных элементов
+{
+    int length = array.Length;
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0) result += 1;
+    }
+    return result;
+}
+
+int [] test = createArray(10);
+PrintNew(test);
+Console.WriteLine();
+Console.WriteLine(EvenNum(test));
