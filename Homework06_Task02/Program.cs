47a2b3970b75cc30abf5084b2f61cd72e40313ba
Dimoklim <dimoklim@ya.ru>
2022-05-19 18:10:18 +0300

    Add Homework06 Task02

---
+++ Homework06_Task02/Program.cs
@@ -0,0 +1,49 @@
+﻿// Найти произведение двух матриц.
+
+int[,] CreateMatrix(int row, int col) // Метод создания и заполнения двумерного массива
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2) // Метод перемножения матриц
+{
+    int[,] product = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            product[i, j] = matrix1[i, j] * matrix2[i, j];
+        }
+    }
+    return product;
+}
+
+void PrintMatrix(int[,] array) // Метод вывода матриц
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix1 = CreateMatrix(3,4);
+int[,] matrix2 = CreateMatrix(3,4);
+int[,] product = MultiplyMatrix(matrix1, matrix2);
+
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+PrintMatrix(product);
