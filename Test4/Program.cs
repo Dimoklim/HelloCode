4a4352f0f4e5cf453ab31e68e7e116888cd4446b
Dimoklim <dimoklim@ya.ru>
2022-05-19 06:39:25 +0300

    Add Test 4, Test 5

---
+++ Test4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Факториал и Фибоначчи
+
+int Factorial(int n) // метод нахождения факториала
+{
+    if (n == 1 || n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+int Fibonacci(int n) // метод нахождения чисел Фибоначчи
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
---
+++ Test5/Program.cs
@@ -0,0 +1,33 @@
+﻿// 
+
+int[,] CreateArray = new int[m, n];
+{
+    return Array;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matr;
+}
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
